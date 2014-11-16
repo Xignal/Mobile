c65e2638151f43194db035d3838c8c5781fbc7d4
Greg Elisara <gregelisara@gmail.com>
2014-11-16 14:57:12 +1300

    added get average for a survey

--- www/app/components/xignal/surveyService.js
+++ www/app/components/xignal/surveyService.js
@@ -6,13 +6,26 @@
 
     function SurveyService($http) {
         var service = {
-            getXignal : getXignal
+            getXignal : getXignal,
+            getSurveyAverage: getSurveyAverage
         };
 
         return service;
 
         function getXignal(id) {
             var url = 'http://onequickq.azurewebsites.net/surveys/' + id;
+            var headers = {"Content-Type": "application/json; charset=utf-8"};
+            return $http({method: 'GET', url: url, headers: headers})
+                .success(function (data, status, headers, config) {
+                    console.log("Data: ", data);
+                    return data;
+                })
+                .error(function (data) {
+                    return data;
+                });
+        }
+        function getSurveyAverage(id) {
+            var url = 'http://onequickq.azurewebsites.net/responses/average/' + id;
             var headers = {"Content-Type": "application/json; charset=utf-8"};
             return $http({method: 'GET', url: url, headers: headers})
                 .success(function (data, status, headers, config) {
